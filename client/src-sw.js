3044fdf8f2ebff94ca98c225d4781ed291b691b3
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-09-08 13:55:53 -0600

    fixing cache issue

--- client/src-sw.js
+++ client/src-sw.js
@@ -30,9 +30,9 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 registerRoute(
   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
+  new CacheFirst({
     // Name of the cache storage.
-    cacheName: 'asset-cache',
+    cacheName: 'jate-image-cache',
     plugins: [
       // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
